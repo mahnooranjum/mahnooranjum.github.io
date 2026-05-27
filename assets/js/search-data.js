907de05e3528f3e98839127ab99089def70102f9
mahnooranjum <40302971+mahnooranjum@users.noreply.github.com>
2026-05-27 14:38:18 +0000

    Deploying to gh-pages from @ mahnooranjum/mahnooranjum.github.io@fdf07e320f2a9f2650084e48fa81aa1ea73b17e0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -130,6 +130,9 @@ ninja.data = [{
           section: "News",},{id: "news-invited-to-serve-on-the-technical-program-committee-tpc-for-ieee-globecom-2026-communication-amp-amp-information-system-security-track",
           title: 'Invited to serve on the Technical Program Committee (TPC) for IEEE GLOBECOM 2026,...',
           description: "",
+          section: "News",},{id: "news-appointed-head-lab-demonstrator-for-elec9123-design-proficiency-may-sep-2026-school-of-electrical-engineering-and-telecommunications-unsw-sydney",
+          title: 'Appointed Head Lab Demonstrator for ELEC9123 Design Proficiency (May–Sep 2026), School of Electrical...',
+          description: "",
           section: "News",},{
         id: 'social-email',
         title: 'email',
